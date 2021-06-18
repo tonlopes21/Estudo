d818e8933d3684a0fe56502aeefcec11dd585398
tonlopes21 <niltonlopes21@gmail.com>
2021-06-18 09:11:22 -0300

    Primeiro commit - Aula javascript

---
+++ Aula_JavaScript/main.js
@@ -0,0 +1,14 @@
+var lista = ["Maçã", "Pêra", "Laranja"]
+
+//var nome = "Nilton Lopes";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade +  "anos");
+//alert (idade + idade2);
+//console.log (nome);
+//console.log (idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert (frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase("Japão", "Brasil"));
+//console.log(frase.toLowerCase("Japão", "Brasil"));
